a7f00d7fbb956b0cdeb432de19d1143fd56e9606
gudaoxuri <i@sunisle.org>
2018-01-10 16:40:00 +0800

    Init

--- all/spark-env.sh
+++
@@ -1,4 +0,0 @@
-export SPARK_MASTER_PORT=7077
-export SPARK_LOCAL_DIRS=/data/spark/data
-export SPARK_LOG_DIR=/data/spark/logs
-export HIVE_HOME=/opt/hive
--- confluent/bootstrap_confluent.sh
+++ confluent/bootstrap_confluent.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/opt/confluent/bin/kafka-server-start /opt/confluent/etc/kafka/server.properties
-/opt/confluent/bin/schema-registry-start /opt/confluent/etc/schema-registry/schema-registry.properties
+nohup /opt/confluent/bin/kafka-server-start /opt/confluent/etc/kafka/server.properties &
+nohup /opt/confluent/bin/schema-registry-start /opt/confluent/etc/schema-registry/schema-registry.properties &
