9b807b057aee78e954967360eca20f60c420a616
gudaoxuri <i@sunisle.org>
2018-01-15 21:40:59 +0800

    Init

--- confluent/bootstrap_confluent.sh
+++ confluent/bootstrap_confluent.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+IP=$(grep "\s${HOSTNAME}$" /etc/hosts | head -n 1 | awk '{print $1}')
+
+cat /opt/confluent/etc/kafka/server.properties.template | sed \
+  -e "s|{{KAFKA_ADVERTISED_HOST_NAME}}|${KAFKA_ADVERTISED_HOST_NAME:-$IP}|g" \
+  -e "s|{{KAFKA_ADVERTISED_PORT}}|${KAFKA_ADVERTISED_PORT:-9092}|g" \
+  -e "s|{{KAFKA_PORT}}|${KAFKA_PORT:-9092}|g" \
+   > /opt/confluent/etc/kafka/server.properties
+
 nohup /opt/confluent/bin/kafka-server-start /opt/confluent/etc/kafka/server.properties &
 sleep 5
 nohup /opt/confluent/bin/schema-registry-start /opt/confluent/etc/schema-registry/schema-registry.properties &
